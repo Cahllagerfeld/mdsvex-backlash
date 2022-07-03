91d1b74f7683455977b30188dfcaef3ac6a65b67
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-03 19:01:32 +0000

    chore: remove test route

--- src/routes/test.svelte
+++
@@ -1,8 +0,0 @@
-<pre class="language-bash">
-<code class="language-bash">
-RUN <span class="token function">bash</span> -lc
-<span class="token string"
-      >"rvm install ruby-2.5.1 &amp;&amp; \ rvm use ruby-ruby-2.5.1 --default"</span
-    ></code
-  >
-</pre>
